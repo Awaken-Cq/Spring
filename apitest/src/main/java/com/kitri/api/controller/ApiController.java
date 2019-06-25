d9fffcf46d67a7b9df126e6e404ba632cb02e599
njtak2514 <njtak2514@gmail.com>
2019-06-25 19:01:44 +0900

    2019.06.25

--- apitest/src/main/java/com/kitri/api/controller/ApiController.java
+++ apitest/src/main/java/com/kitri/api/controller/ApiController.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,57 +19,84 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
-@ResponseBody
 public class ApiController {
-
+	
+	//produces="application/json;charset=UTF-8", 
 		private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
 	
 		@RequestMapping(value="/enter.api", method =RequestMethod.GET)
-		public String enter(@RequestParam(name = "key")String msg) {
+		@ResponseBody
+		public String enter(@RequestParam(name = "key") String msg) {
 			System.out.println("entered");
 			logger.info(msg);
-			String apikey = "qldeV%2BL5Ff%2BFi%2BJisZxRFyc1KDitxcPmNkhuwOjk6c7xQDVITEe0oDrh3XFd98iqnW89ky8RMDhQkQIb48h3%2BQ%3D%3D";
-			String tourapikey = "";
-			String result = apiexc(apikey);
+			String takapikey = "qldeV%2BL5Ff%2BFi%2BJisZxRFyc1KDitxcPmNkhuwOjk6c7xQDVITEe0oDrh3XFd98iqnW89ky8RMDhQkQIb48h3%2BQ%3D%3D";
+			String sehyunapikey = "eVXAsaFVfjgGhAQfsYx28kYnz0nbDxSuCQlP9TnGl8ntcFd3V%2Byjhkh%2BUfuNwRDIZYBgDnL3Cm0BzM8Ezw73mQ%3D%3D";
 			
+			String periodurlStr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?"
+					+ "numOfRows=10&"
+					+ "pageNo=1&"
+					+ "MobileOS=ETC&"
+					+ "MobileApp=AppTest&"
+					+ "listYN=Y&"
+					+ "arrange=A&"
+					+ "contentTypeId=15&"
+					+ "cat1=A02&"
+					+ "cat2=A0207&"
+					+ "cat3=A02070100&"
+				/* + "keyword=%EA%B0%95%EC%9B%90" */
+					+ "_type=json&"
+					+ "ServiceKey="+takapikey;
+			
+			String categoryurlStr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/categoryCode?"
+					
+							+ "MobileOS=ETC&"
+							+ "MobileApp=AppTest&"
+							+ "contentTypeId=15&"
+							+ "cat1=A02&"
+							+ "cat2=A0208&"
+							+ "cat3=A02080300&"
+							+ "_type=json&"
+							+ "ServiceKey=" + takapikey;
+			
+			logger.info("----------------------------------");
+			String result = apiexc(periodurlStr);
+			//model.addAttribute("json", result);
+			logger.info(result);
+			logger.info("----------------------------------");
 			
 			return result;
 			
 			
 		}
-	
 		
-		public String apiexc(String apikey) {
-		String result = "";
-		BufferedReader br = null;
 		
-		String urlStr = "http://www.culture.go.kr/openapi/rest/"
-				+ "publicperformancedisplays/"
-				+ "period?from=20190501&to=20190801&cPage=1&"
-				+ "rows=10&place=&gpsxfrom=&gpsyfrom=&gpsxto=&"
-				+ "gpsyto=&keyword=&sortStdr=1&serviceKey="+ apikey;
-		URL url;
-		try {
-			url = new URL(urlStr);
-			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("GET");
-			br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-			
-			String line;
-			while((line = br.readLine()) != null) {
-				result += result + line + "/n";
+		public String apiexc(String urlStr) {
+			String result = "";
+			BufferedReader br = null;
+
+			URL url;
+			try {
+				url = new URL(urlStr);
+				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod("GET");
+				br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+				
+				String line;
+				while((line = br.readLine()) != null) {
+					result += result + line + "\n";
+				}
+				
+				
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			logger.info(result);
+			return result;
 			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		return result;
 		
-		}
 		
 }
--- apitest/src/main/webapp/index.jsp
+++ apitest/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <script	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <c:set var="root" value="${pageContext.request.contextPath}"/>
@@ -15,15 +16,23 @@ $(function() {
 	$.ajax({
 		url : "${root}/api/enter.api",
 		type : "get",
+		dataType : "json",
 		data : {"key":"keykey"},
 		success :function(data){
-			alert(data);
-			$("#result").append(data);
+			console.log(data);
+			var items = data.response.body.items;
+			var view = '';
+			console.log('item = '+items.key("item").length);
+			console.log('length = '+items[0].areacode);
+			
+			
+			
+			
 		}
 				
 	});
 		
-		
+		return;
 	});
 	
 	
